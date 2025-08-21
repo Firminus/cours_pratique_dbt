32a5e3a4a70d55b5903e043faf28028547c4e5a9
firminbadiel@cciat.local <firmin badiel>
2025-08-21 12:56:41 +0200

    fait le 21/08/2025

--- analyses/analyse_exploratoire.sql
+++ analyses/analyse_exploratoire.sql
@@ -1,34 +1,62 @@
-/*SELECT * FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet' limit 10 ;*/
+-- /*SELECT * FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet' limit 10 ;*/
 
-/************VENDOR ID*****************/
+
+-- -- .read './analyses/analyse_exploratoire.sql'
+
+
+-- SELECT COUNT(*)
+-- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- ;
+
+-- SUMMARIZE 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- ;
+
+-- DESCRIBE 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- ;
+
+-- /************************VISUALISATION**************/
+-- SELECT *
+-- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- WHERE trip_distance <=0
+-- LIMIT 10 ;
+
+-- /************VENDOR ID*****************/
 -- SELECT VendorID,COUNT(*) as trips_count
 -- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
 -- GROUP BY VendorID ;
 
 
-/************************payment_type**************/
+-- /************************payment_type**************/
 -- SELECT payment_type , COUNT(*) as trips_count
 -- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
 -- GROUP BY payment_type;
--- .read './analyses/analyse_exploratoire.sql'
-/************************payment_type**************/
-SELECT *
-FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
-WHERE trip_distance <=0
-LIMIT 10 ;
 
-with source_data as (
-    select *
-    from {{ source('tlc_taxi_trips', 'raw_yellow_tripdata') }}
-)
+-- /************************compter  les occu par rapport**************/
+-- SELECT  COUNT(*) as trips_count
+-- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- WHERE tpep_pickup_datetime >tpep_dropoff_datetime
+-- ;
 
-select
-    pickup_datetime,
-    dropoff_datetime,
-    passenger_count,
-    trip_distance,
-    payment_type,
-    fare_amount,
-    total_amount
-from source_data
-limit 100 ;
+
+-- /************************affichage des dates**************/
+-- SELECT  *
+-- FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
+-- WHERE tpep_pickup_datetime >tpep_dropoff_datetime
+-- ;
+
+
+WITH source_data as (
+    SELECT *
+    FROM --{{ source('tlc_taxi_trips', 'raw_yellow_tripdata') }}
+'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet')
+
+SELECT
+    tpep_pickup_datetime
+    -- dropoff_datetime,
+    -- passenger_count,
+    -- trip_distance,
+    -- payment_type,
+    -- fare_amount,
+    -- total_amount
+FROM source_data
+LIMIT 100 ;
